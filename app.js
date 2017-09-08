3d7b1f08e60b1c22016a627a525d31276b3269e8
Larry Sharette <larry.sharette1988@email.bakersfieldcollege.edu>
2017-09-08 11:23:36 -0700

    fixing port issue for heroku app deployment.

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ app.get('/', function(req, res){
     res.sendfile('layout.html');
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log('The app is running');
 })
